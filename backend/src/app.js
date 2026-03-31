53120de7b2a43e0bced2d3bf2ce305e21be5fa66
vedant Chaudhari <vedantchaudhari3817@gmail.com>
2026-03-31 10:07:12 +0530

    initial commit

---
+++ backend/src/app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", (process.env.PORT || 8000));
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+app.use("/api/v1/users", userRoutes);
+
+app.get("/home", (req, res) => {
+    return res.json("Welcome to StackMeet API");
+});
+
+const start = async () => {
+    const connectionDb = await mongoose.connect("mongodb+srv://stackmeet:stackmeet%40123@stackmeetcluster.k9xfdis.mongodb.net/")
+    console.log(`MONGO Connected DB Host: ${connectionDb.connection.host}`);
+    server.listen(app.get("port"), () => {
+        console.log("LISTENING ON PORT 8000");
+    });
+}
+
+start();
+
+
